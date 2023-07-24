0b48b47932371d0e8f68af51665ff34f217b937e
EYS06 <elyas.achmadudin30@gmail.com>
2023-07-24 17:44:26 +0700

    update

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const articleLinks = document.querySelectorAll('.article-link');
-  const articleContent = document.querySelector('#articleContent');
-
-  articleLinks.forEach(function(link) {
-    link.addEventListener('click', function(event) {
-      event.preventDefault();
-
-      const articleUrl = link.getAttribute('data-article');
-
-      fetch(articleUrl)
-        .then(response => response.text())
-        .then(content => {
-          articleContent.innerHTML = content;
-        })
-        .catch(error => {
-          console.error('Error:', error);
-        });
-    });
-  });
-});
+// Fungsi untuk mengambil konten dari berbagai alamat menggunakan AJAX
+function ambilKonten(url) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      // Set konten yang diambil dari url ke elemen dengan id "konten"
+      document.getElementById("konten").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", url, true);
+  xhttp.send();
+}
