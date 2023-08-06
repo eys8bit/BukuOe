f2d48c1d9a7e068778f37a9e374bfe14eaa41da2
EYS06 <elyas.achmadudin30@gmail.com>
2023-08-06 17:15:59 +0700

    update

--- js/menuGeserDanAmbil_Iframe.js
+++ js/menuGeserDanAmbil_Iframe.js
@@ -7,3 +7,11 @@ function loadIframe(url) {
   iframe.src = url;
   document.querySelector(".menu").classList.toggle("active");
 }
+
+// hanya untuk uji web yang akan di tambah
+/* 
+const iframeElement = document.getElementById("ambilIframe");
+const contentURL = "z_bukus/blender/blender_page/00_catatan.html"; // Ganti dengan alamat yang sesuai
+iframeElement.src = contentURL;
+*/
+/* end */
