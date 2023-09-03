969d1423742e0658ed8b05c4bfeb680e18f69078
EYS06 <elyas.achmadudin30@gmail.com>
2023-09-03 22:14:33 +0700

    update

--- js/menuGeserDanAmbil_Iframe.js
+++ js/menuGeserDanAmbil_Iframe.js
@@ -9,6 +9,7 @@ function loadIframe(url) {
 }
 
 //function untuk langsung ke shortcut link hanya untuk pc
+/**
 const tombolJalankan = document.getElementById("tombolJalankan");
 
 tombolJalankan.addEventListener("click", function() {
@@ -16,6 +17,28 @@ tombolJalankan.addEventListener("click", function() {
   const contentURL = "z_bukus/link_web/link_web.html"; // Ganti dengan alamat yang sesuai
   iframeElement.src = contentURL;
 });
+/**/
+
+// Membuat objek media query
+const mediaQuery = window.matchMedia("(min-width: 991px)"); // Sesuaikan dengan kondisi yang Anda inginkan
+
+// Fungsi yang akan dijalankan saat kondisi media query sesuai
+function handleMediaQueryChange(event) {
+  if (event.matches) {
+    // Kode yang akan dijalankan saat media query sesuai (lebih dari 991px)
+    tombolJalankan.addEventListener("click", function () {
+      const iframeElement = document.getElementById("ambilIframe");
+      const contentURL = "z_bukus/link_web/link_web.html"; // Ganti dengan alamat yang sesuai
+      iframeElement.src = contentURL;
+    });
+  }
+}
+
+// Menjalankan fungsi pertama kali saat halaman dimuat
+handleMediaQueryChange(mediaQuery);
+
+// Menambahkan event listener untuk memantau perubahan media query
+mediaQuery.addListener(handleMediaQueryChange);
 
 // hanya untuk uji web yang akan di tambah
 /* 
